8387add1747673da12ca4fe6ce9614567344c232
teamkotlin <muhammadramzan6254@gmail.com>
2025-02-24 12:14:33 +0500

    life cycle hook onMounted added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld />
+  <VueHooks />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import VueHooks from "./components/VueHooks.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    VueHooks,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/VueHooks.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref, onMounted } from "vue";
+const elementRef = ref(null);
+onMounted(() => {
+  elementRef.value.textContent = "Using onMounted Life Cycle Method";
+});
+</script>
+<template>
+  <h1>Life Cycle Hooks</h1>
+  <p ref="elementRef">Hello</p>
+</template>
