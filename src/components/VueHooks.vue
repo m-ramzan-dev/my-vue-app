d36a3a3e14d75b2acb37db1e37c762b3676bd5eb
teamkotlin <muhammadramzan6254@gmail.com>
2025-02-26 15:33:12 +0500

    attribute binding

--- src/components/VueHooks.vue
+++ src/components/VueHooks.vue
@@ -4,8 +4,38 @@ const elementRef = ref(null);
 onMounted(() => {
   elementRef.value.textContent = "Using onMounted Life Cycle Method";
 });
+
+const message = ref("Learning Vue Js");
+const tooltip = ref("Nice 👍");
+const isRed = ref(false);
+const color = ref("green");
+
+function reverse() {
+  message.value = message.value.split("").reverse().join("");
+}
+function append() {
+  message.value += "!";
+}
+function toggleRed() {
+  isRed.value = !isRed.value;
+}
+function toggleColor() {
+  color.value = color.value == "green" ? "blue" : "green";
+}
 </script>
 <template>
   <h1>Life Cycle Hooks</h1>
   <p ref="elementRef">Hello</p>
+  <h1 @click="toggleColor" :style="{ color }">{{ message }}</h1>
+  <span :title="tooltip" @click="toggleRed" :class="{ red: isRed }"
+    >Hover Here to See ToolTip</span
+  >
+  <button @click="reverse">Reverse Message</button>
+  <button @click="append">Append !</button>
 </template>
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>
