3c54091151ea7ef8f9bfd0ffda30c808410c30e9
teamkotlin <muhammadramzan6254@gmail.com>
2025-03-02 15:53:52 +0500

    define props and defineemits done

--- vue-event-app/src/App.vue
+++ vue-event-app/src/App.vue
@@ -3,7 +3,14 @@
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="i in 8" :key="i" />
+      <EventCard
+        v-for="i in 8"
+        :key="i"
+        title="Vue Conference 2024"
+        date="2024-05-01"
+        description="Developer Conference for Vue and JavaScript"
+        @register="console.log('Registering...', i)"
+      />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
--- vue-event-app/src/components/EventCard.vue
+++ vue-event-app/src/components/EventCard.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="bg-white border border-gray-200 rounded-md">
-    <h3 class="font-medium text-xl p-4 border-b border-gray-200">What</h3>
-    <p class="p-4 border-b border-gray-200">When</p>
-    <p class="p-4">Description</p>
+    <h3 class="font-medium text-xl p-4 border-b border-gray-200">
+      {{ title }}
+    </h3>
+    <p class="p-4 border-b border-gray-200">{{ date }}</p>
+    <p class="p-4">{{ description }}</p>
+    <section class="p-4 flex justify-end">
+      <button
+        class="hover:bg-gray-100 border text-sm font-medium border-gray-200 rounded-full px-3 py-1"
+        @click="$emit('register')"
+      >
+        Register
+      </button>
+    </section>
   </div>
 </template>
+<script setup>
+defineProps({
+  title: String,
+  date: String,
+  description: String,
+});
+defineEmits(["register"]);
+</script>
