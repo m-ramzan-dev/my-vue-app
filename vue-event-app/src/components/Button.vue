7ef5795c9c2835b903b14c6e72f56d01bb402931
teamkotlin <muhammadramzan6254@gmail.com>
2025-03-02 16:10:58 +0500

    button coponent separated with emit

---
+++ vue-event-app/src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button
+    class="hover:bg-gray-100 border text-sm font-medium border-gray-200 rounded-full px-3 py-1"
+    @click="$emit('click')"
+  >
+    {{ label }}
+  </button>
+</template>
+<script setup>
+defineProps({
+  label: String,
+});
+defineEmits(["click"]);
+</script>
--- vue-event-app/src/components/EventCard.vue
+++ vue-event-app/src/components/EventCard.vue
@@ -6,16 +6,12 @@
     <p class="p-4 border-b border-gray-200">{{ date }}</p>
     <p class="p-4">{{ description }}</p>
     <section class="p-4 flex justify-end">
-      <button
-        class="hover:bg-gray-100 border text-sm font-medium border-gray-200 rounded-full px-3 py-1"
-        @click="$emit('register')"
-      >
-        Register
-      </button>
+      <Button label="Register" @click="$emit('register')" />
     </section>
   </div>
 </template>
 <script setup>
+import Button from "./Button.vue";
 defineProps({
   title: String,
   date: String,
