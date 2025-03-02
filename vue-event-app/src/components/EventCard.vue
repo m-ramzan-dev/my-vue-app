a97917c88832f4bb017e3d0e10f7faef9da7bf42
teamkotlin <muhammadramzan6254@gmail.com>
2025-03-02 15:34:30 +0500

    event card designed

---
+++ vue-event-app/src/components/EventCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-white border border-gray-200 rounded-md">
+    <h3 class="font-medium text-xl p-4 border-b border-gray-200">What</h3>
+    <p class="p-4 border-b border-gray-200">When</p>
+    <p class="p-4">Description</p>
+  </div>
+</template>
