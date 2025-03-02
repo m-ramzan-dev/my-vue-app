ea639bc777d8cb08465fee1e1545adf15e403b81
teamkotlin <muhammadramzan6254@gmail.com>
2025-03-02 15:34:00 +0500

    event card designed

--- product-management-app/src/App.vue
+++ product-management-app/src/App.vue
@@ -2,7 +2,17 @@
 import { RouterLink, RouterView } from "vue-router";
 </script>
 
-<template>
+<template >
+  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
+    <form class="w-full max-w-96">
+      <input
+        type="text"
+        class="w-full pl-8 bg-background"
+        placeholder="Search"
+      />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
--- product-management-app/vite.config.ts
+++ product-management-app/vite.config.ts
@@ -1,4 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -10,6 +12,14 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  css:{
+    postcss: {
+      plugins: [
+        tailwind(),
+        autoprefixer()
+      ]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
