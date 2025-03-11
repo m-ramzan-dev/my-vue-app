ada8a108418271fd662f49a9e8b24fcbdea85a6f
teamkotlin <muhammadramzan6254@gmail.com>
2025-03-12 02:35:21 +0500

    rendering tabs done

--- vue-settings/src/App.vue
+++ vue-settings/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import type { Tab, TabKey } from "./types";
+import LinkTab from "./components/LinkTab.vue";
 const tabs: Tab[] = [
   {
     key: "General",
@@ -19,7 +20,19 @@ const currentTab = ref<TabKey>("General");
 </script>
 
 <template>
-  <div>
-    <h1>Vue 3 + Vite + TypeScript</h1>
-  </div>
+  <main class="max-w-2xl mx-auto px-4">
+    <nav
+      class="font-medium text-center text-gray-500 border-b border-gry-200mb-4"
+    >
+      <ul class="flex flex-wrap -mb-px">
+        <li v-for="tab in tabs" :key="tab.key">
+          <LinkTab
+            :tab="tab"
+            :current-tab="currentTab"
+            @click="currentTab = tab.key"
+          />
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
--- vue-settings/src/components/LinkTab.vue
+++ vue-settings/src/components/LinkTab.vue
@@ -1,8 +1,24 @@
-<template>Link</template>
+<template>
+  <button
+    @click="$emit('click')"
+    :class="{
+      'p-4 border-b-2': true,
+      'border-transparent': !isActive,
+      'border-b border-blue-600 text-blue-600': isActive,
+    }"
+  >
+    {{ tab.label }}
+  </button>
+</template>
 <script setup lang="ts">
+import { computed } from "vue";
 import type { Tab } from "../types";
-defineProps<{
+const props = defineProps<{
   currentTab: string;
   tab: Tab;
 }>();
+defineEmits<{
+  click: [];
+}>();
+const isActive = computed(() => props.currentTab === props.tab.key);
 </script>
