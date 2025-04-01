1c7447ad178c6020f0dedb5a3220852777a7cc92
Muhammad Ramzan <muhammad-ramzan@Apples-MacBook-Pro.local>
2025-04-01 18:51:45 +0500

    centralized init function added in useSettings composables

--- vue-settings/src/composables/useSettings.ts
+++ vue-settings/src/composables/useSettings.ts
@@ -1,5 +1,9 @@
 import { ref } from "vue"
 
+const init = <T>(key:string,defaults:T)=>{
+    const stored  = localStorage.getItem(key);
+    return stored !== null ? JSON.parse(stored) : defaults
+}
 interface GeneralSettings{
     name:string,
     email:string,
@@ -13,29 +17,27 @@ interface NotificationSettings{
     sms:boolean
 }
 
-const notifications = ref<NotificationSettings>({
+const notifications = ref<NotificationSettings>(init('notifications',{
     email:false,
     sms:false
-});
+}));
 interface PrivacySettings{
     visibility:Visibility,
     searchEngineIndesing: boolean
 }
 type Visibility = "public"|"private"
-const privacy = ref<PrivacySettings>({
+const privacy = ref<PrivacySettings>(init('privacy',{
     visibility:"public",
     searchEngineIndesing:false
-});
-const general = ref<GeneralSettings>((()=>{
-    const stored  = localStorage.getItem('general');
-    return stored !== null ? JSON.parse(stored) : {
-        name:"",
-        email:"",
-        gender:"Male",
-        country:"Pakistan",
-        about:""
-    }
-})());
+}));
+
+const general = ref<GeneralSettings>(init('general',{
+    name:"",
+    email:"",
+    gender:"Male",
+    country:"Pakistan",
+    about:""
+}));
 export function useSettings(){
     return {
         general,
