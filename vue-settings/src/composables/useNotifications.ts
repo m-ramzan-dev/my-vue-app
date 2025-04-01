4fba9837bfd54a8b316527d0b6f86b32ad6a840d
Muhammad Ramzan <muhammad-ramzan@Apples-MacBook-Pro.local>
2025-04-01 19:42:36 +0500

    ading composables for adding notifications

---
+++ vue-settings/src/composables/useNotifications.ts
@@ -0,0 +1,25 @@
+import { ref } from 'vue';
+interface Notification{
+    id:number,
+    message:string,
+}
+const notifications = ref<Notification[]>([]);
+
+const addNotification = (message:string)=>{
+    const id =  Date.now();
+    notifications.value.push({id,message});
+    setTimeout(() => {
+        removeNotification(id)
+    }, 5000);
+}
+const removeNotification = (id:number)=>{
+    notifications.value = notifications.value.filter((notifications)=>notifications.id !== id);
+}
+
+export function useNotifications(){
+    return{
+        notifications,
+        addNotification,
+        removeNotification
+    }
+}
