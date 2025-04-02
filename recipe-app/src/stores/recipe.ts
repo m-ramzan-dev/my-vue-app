d1a92788e144127b9a7428a0185acf22650a3b33
Ramzan <mhdramzan.dev@gmail.com>
2025-04-02 17:56:20 +0500

    recipe store added to the recipe project

--- recipe-app/src/stores/recipe.ts
+++ recipe-app/src/stores/recipe.ts
@@ -8,5 +8,27 @@ interface Recipe {
 
 export const useRecipeStore = defineStore('recipeStore',()=>{
     const recipes = ref<Recipe[]>([]);
-    return {recipes}
+
+    const addRecipe = (recipe: Recipe) =>{
+        recipes.value.push(recipe);
+    }
+    const removeRecipe = (id: number) =>{
+        recipes.value = recipes.value.filter(recipe => recipe.id !== id);
+    }
+    const getRecipe = (id: number) =>{
+        return recipes.value.find(recipe => recipe.id === id);
+    }
+    const getAllRecipes= () =>{
+        return recipes.value;
+    }
+    const clearRecipes = () =>{
+        recipes.value = [];
+    }
+    const updateRecipe = (id: number, updatedRecipe: Recipe) =>{
+        const index = recipes.value.findIndex(recipe => recipe.id === id);
+        if (index !== -1) {
+            recipes.value[index] = { ...recipes.value[index], ...updatedRecipe };
+        }
+    }
+    return {recipes,addRecipe,removeRecipe,getRecipe,getAllRecipes,clearRecipes,updateRecipe}
 });
