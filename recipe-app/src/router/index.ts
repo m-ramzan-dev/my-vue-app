5297cb3c99e1567dddb17f0a6a9ab536092b5521
Muhammad Ramzan <muhammad-ramzan@Apples-MacBook-Pro.local>
2025-04-02 13:16:37 +0500

    recipe page added with params attribute

--- recipe-app/src/router/index.ts
+++ recipe-app/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,6 +20,11 @@ const router = createRouter({
       name: 'add-favorites',
       component: () => import('../views/AddFavoriteView.vue'),
     },
+    {
+      path:'/recipe/:id',
+      name: 'recipe',
+      component: () => import('../views/RecipeView.vue'),
+    }
   ],
 })
 
---
+++ recipe-app/src/views/RecipeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Recipe {{ route.params.id }}</div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+const route = useRoute();
+</script>
