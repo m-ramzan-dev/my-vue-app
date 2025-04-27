1f177f3d1cc5563800024dfe05b4aa0ab1d99e1b
Ramzan <mhdramzan.dev@gmail.com>
2025-04-27 14:21:58 +0500

    recipe list shown

--- recipe-app/src/views/HomeView.vue
+++ recipe-app/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useRecipeStore } from "../stores/recipe";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const recipeStore = useRecipeStore();
 const name = ref("");
@@ -10,6 +10,9 @@ function addRecipe() {
   name.value = "";
   description.value = "";
 }
+const recipes = computed(() => {
+  return recipeStore.getAllRecipes();
+});
 </script>
 
 <template>
@@ -20,6 +23,18 @@ function addRecipe() {
       recipes.
     </p>
     <div>
+      <h1>Recipes</h1>
+      <div>
+        <ul>
+          <li v-for="recipe in recipes" :key="recipe.id">
+            <RouterLink :to="{ name: 'recipe', params: { id: recipe.id } }">
+              {{ recipe.name }}
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div>
       <h1>Add Recipe</h1>
       <div>
         <input type="text" placeholder="Recipe Name" v-model="name" />
@@ -27,7 +42,7 @@ function addRecipe() {
       <div>
         <textarea name="description" id="" v-model="description"></textarea>
       </div>
-      <button onclick="addRecipe" type="submit">Submit</button>
+      <button @click="addRecipe" type="button">Submit</button>
     </div>
   </main>
   <header>
